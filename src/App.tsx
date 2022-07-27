ab9f685d6239dc18ba7786dce696697298b299ba
Yuvaraj V <yuvaraj.v@indiumsoft.com>
2022-07-27 12:07:17 +0530

    adde droutes and common navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import AppHeader from './components/AppHeader/AppHeader';
 import SeatingConfig from './pages/SeatingConfig/SeatingConfig.lazy';
 import Signin from './pages/Signin/Signin.lazy';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader/>
       <Routes>
         <Route path="/login" element={<Signin />} />
-        <Route path="/configs">
-          <Route path="/seating" element={<SeatingConfig />}></Route>
+        <Route path="/config">
+          <Route path="seating" element={<SeatingConfig />}></Route>
         </Route>
       </Routes>
     </div>
